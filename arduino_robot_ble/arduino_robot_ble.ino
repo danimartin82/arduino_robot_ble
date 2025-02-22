1df69ca4df744c888ea874852b9276442d122139
Dani Martin <dani_martin_82@hotmail.com>
2025-02-22 16:55:44 +0100

    Hemos puesto sensor ultrasonidos

--- arduino_robot_ble/arduino_robot_ble.ino
+++ arduino_robot_ble/arduino_robot_ble.ino
@@ -1,25 +1,45 @@
+/******************************************************************************
+* L298N motor control
+* IN1 -> PIN  8
+* IN2 -> PIN  9
+* IN3 -> PIN 10
+* IN4 -> PIN 11
+*
+*  HC-05 bluetooth interface
+*  RX -> PIN 4
+*  TX -> PIN 5
+*  EN -> PIN 2
+* 
+
+* Echo -> PIN 12
+* Trigger -> PIN 13
+*******************************************************************************/
+
 #include <SoftwareSerial.h>
 
+// BLE Commands
 # define FORDWARD    'F'
 # define BACKWARD    'B'
 # define LEFT        'L'
 # define RIGHT       'R'
 #define PAUSE        'P'
+
 // Motor A
-//#define MOTOR_ENA    10
 #define MOTOR_IN1     8
 #define MOTOR_IN2     9
 
 // Motor B
 #define MOTOR_IN3    10
 #define MOTOR_IN4    11
-//#define MOTOR_ENB     9
 
 //BLE
 #define BLE_rxPin     4
 #define BLE_txPin     5
 #define BLE_enablePin 2
-#define BLE_VccPin    3
+
+// Ultrasonidos
+#define EchoPin      12
+#define TriggerPin   13
 
 
 SoftwareSerial BT (BLE_rxPin,BLE_txPin); 
@@ -35,53 +55,79 @@ void setup() {
   pinMode (MOTOR_IN3, OUTPUT);
   pinMode (MOTOR_IN4, OUTPUT);
   pinMode(BLE_enablePin, OUTPUT);
-  pinMode(BLE_VccPin, OUTPUT);
 
   // Enable BLE
+  Serial.println("Levantando el modulo HC-05");
   digitalWrite(BLE_enablePin, HIGH);
   delay(500);
 
   Serial.begin(9600);
   BT.begin(9600);
 
-  // Power on BLE
-  Serial.println("Levantando el modulo HC-05");
-  digitalWrite(BLE_VccPin, HIGH);
 }
+char cmd;
+char direcion;
+int distancia;
 
 void loop() {
+
+  distancia = ping();
+  Serial.print("Distancia: ");
+  Serial.println(distancia);
+  Serial.println(direcion);
+  if ((direcion == FORDWARD) && (distancia < 20))
+  {
+    parar();
+  }
+
+
   if(BT.available())
   {
-    char cmd = BT.read();
+    cmd = BT.read();
+     Serial.println("cambio cmd");
     //Serial.write(cmd);
+
     switch(cmd)
     {
       case FORDWARD:
       {
-        adelante();
+        if (distancia > 20)
+        {
+          direcion = cmd;
+          adelante();
+        }
+        else
+        {
+          direcion = cmd;
+          parar();
+        }
+
       }
       break;
       case BACKWARD:
       {
+        direcion = cmd;
         atras();
       }
       break;
       case LEFT:
       {
+        direcion = cmd;
         izquierda();
       }
       break;
       case RIGHT:
       {
+        direcion = cmd;
         derecha();
       }
       break;
       case PAUSE:
       {
+        direcion = cmd;
         parar();
       }
       break;
-      
       
     }
   }
@@ -91,6 +137,9 @@ void loop() {
 
     BT.write(Serial.read());
   }
+
+
+  delay(200);
 
 }
 
@@ -103,7 +152,7 @@ void adelante(void)
   // Adelante motor A
   digitalWrite(MOTOR_IN1, HIGH);
   digitalWrite(MOTOR_IN2, LOW);
-  
+
   // Adelante motor B
   digitalWrite(MOTOR_IN3, LOW);
   digitalWrite(MOTOR_IN4, HIGH);
@@ -165,5 +214,22 @@ void parar(void)
  digitalWrite(MOTOR_IN3, LOW);
  digitalWrite(MOTOR_IN4, LOW);
 
+}
+
+
+
+int ping(void) {
+  long duration, distanceCm;
+  
+  digitalWrite(TriggerPin, LOW);  //para generar un pulso limpio ponemos a LOW 4us
+  delayMicroseconds(4);
+  digitalWrite(TriggerPin, HIGH);  //generamos Trigger (disparo) de 10us
+  delayMicroseconds(10);
+  digitalWrite(TriggerPin, LOW);
+  
+  duration = pulseIn(EchoPin, HIGH);  //medimos el tiempo entre pulsos, en microsegundos
+  
+  distanceCm = duration * 10 / 292/ 2;   //convertimos a distancia, en cm
+  return distanceCm;
 }
 
